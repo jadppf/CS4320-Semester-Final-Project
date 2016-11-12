6a2727b2de0c917d70f4a37fe7ac96d0168a8a51
William Morrison <wmorrison96@yahoo.com>
2016-11-11 18:27:50 -0600

    commit 7

--- Site/template/navigation.php
+++ Site/template/navigation.php
@@ -1,14 +1,13 @@
-<nav class="navbar navbar-default">
-	<div class="container">
-		<div class="container-fluid">
-			<ul class="nav navbar-nav">
-				<!--<?php nav_main($dbc,$pageid); ?>-->
-				<li><a href="">Upload</a></li>
-				<li><a href="">Browsing</a></li>
-				<li><a href="">Search</a></li>
-				<li><a href="">Setting</a></li>
-				<li><a href="logout.php">Logout</a></li>
-			</ul>				
-		</div>
-	</div> 
-</nav><!-- END nav-->
+<nav class="indigo" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Software Engineering</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Git Hub</a></li>
+<!--         <li><input id="search"><i class="material-icons">search</i></li> -->
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
